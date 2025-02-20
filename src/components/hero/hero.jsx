ff55218287086fb60e5a51ecde99141adf8ab657
Gokul VR <106941614+Gokul-VR@users.noreply.github.com>
2025-02-20 13:21:36 +0530

    Add files via upload

--- src/components/hero/hero.jsx
+++ src/components/hero/hero.jsx
@@ -29,94 +29,94 @@ export default function Hero() {
   }, []);
 
   return (
-    // <Box
-    //   ref={heroRef}
-    //   sx={{
-    //     minHeight: "100vh",
-    //     display: "flex",
-    //     alignItems: "center",
-    //     justifyContent: "center",
-    //   }}
-    // >
-    //   <Container maxWidth="lg">
-    //     <Grid
-    //       container
-    //       spacing={4}
-    //       alignItems="center"
-    //       //   sx={{ marginTop: { xs: -10, md: 0 } }}
-    //     >
-    //       <Grid
-    //         item
-    //         xs={12}
-    //         md={7}
-    //         sx={{
-    //           display: "flex",
-    //           justifyContent: "center",
-    //           position: "relative",
-    //           overflow: "visible",
-    //           zIndex: 10,
-    //         }}
-    //       >
-    //         {/* <div
-    //           className="home__img"
-    //           style={{
-    //             backgroundImage: `url(${myImage})`,
-    //           }}
-    //         ></div> */}
-    //         <Lanyard position={[0, 0, 20]} gravity={[0, -40, 0]} />
-    //       </Grid>
-    //       <Grid
-    //         item
-    //         xs={12}
-    //         md={5}
-    //         sx={{
-    //           //  marginBottom: { xs: 8, md: 0 },
-    //           textAlign: { xs: "center", md: "left" },
-    //         }}
-    //       >
-    //         <motion.div
-    //           initial={{ opacity: 0, x: -20 }}
-    //           animate={{ opacity: 1, x: 0 }}
-    //           transition={{ duration: 0.8 }}
-    //         >
-    //           <motion.div
-    //             initial={{ opacity: 0, y: 20 }}
-    //             animate={{ opacity: 1, y: 0 }}
-    //             whileHover={{ rotateY: 15, rotateX: 15, scale: 1.05 }}
-    //             whileTap={{ rotateY: 0, rotateX: 0, scale: 0.95 }}
-    //             transition={{
-    //               type: "spring",
-    //               stiffness: 300,
-    //               damping: 20,
-    //               duration: 0.8,
-    //             }}
-    //           >
-    //             <Typography
-    //               sx={{ marginTop: 5, fontSize: { xs: "2.6rem", md: "4rem" } }}
-    //             >
-    //               <SplitText text="Hello" delay={60} />
-    //             </Typography>
-    //           </motion.div>
-    //           <Typography sx={{ fontSize: { xs: "1.7rem", md: "2.5rem" } }}>
-    //             I'm Gokul,
-    //           </Typography>
-    //           <Typography sx={{ fontSize: { xs: "1.8rem", md: "2.5rem" } }}>
-    //             a Software{" "}
-    //             <span style={{ display: "inline-block" }}>
-    //               <Typewriter
-    //                 options={{
-    //                   strings: [" Developer", " Engineer"],
-    //                   autoStart: true,
-    //                   loop: true,
-    //                 }}
-    //               />
-    //             </span>
-    //           </Typography>
-    //         </motion.div>
-    //       </Grid>
-    //     </Grid>
-    //   </Container>
-    // </Box>
+    <Box
+      ref={heroRef}
+      sx={{
+        minHeight: "100vh",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      <Container maxWidth="lg">
+        <Grid
+          container
+          spacing={4}
+          alignItems="center"
+          //   sx={{ marginTop: { xs: -10, md: 0 } }}
+        >
+          <Grid
+            item
+            xs={12}
+            md={5}
+            sx={{
+              //  marginBottom: { xs: 8, md: 0 },
+              textAlign: { xs: "center", md: "left" },
+            }}
+          >
+            <motion.div
+              initial={{ opacity: 0, x: -20 }}
+              animate={{ opacity: 1, x: 0 }}
+              transition={{ duration: 0.8 }}
+            >
+              <motion.div
+                initial={{ opacity: 0, y: 20 }}
+                animate={{ opacity: 1, y: 0 }}
+                whileHover={{ rotateY: 15, rotateX: 15, scale: 1.05 }}
+                whileTap={{ rotateY: 0, rotateX: 0, scale: 0.95 }}
+                transition={{
+                  type: "spring",
+                  stiffness: 300,
+                  damping: 20,
+                  duration: 0.8,
+                }}
+              >
+                <Typography
+                  sx={{ marginTop: 5, fontSize: { xs: "2.6rem", md: "4rem" } }}
+                >
+                  <SplitText text="Hello" delay={60} />
+                </Typography>
+              </motion.div>
+              <Typography sx={{ fontSize: { xs: "1.7rem", md: "2.5rem" } }}>
+                I'm Gokul,
+              </Typography>
+              <Typography sx={{ fontSize: { xs: "1.8rem", md: "2.5rem" } }}>
+                a Software{" "}
+                <span style={{ display: "inline-block" }}>
+                  <Typewriter
+                    options={{
+                      strings: [" Developer", " Engineer"],
+                      autoStart: true,
+                      loop: true,
+                    }}
+                  />
+                </span>
+              </Typography>
+            </motion.div>
+          </Grid>
+          <Grid
+            item
+            xs={12}
+            md={7}
+            sx={{
+              display: "flex",
+              justifyContent: "center",
+              position: "relative",
+              overflow: "visible",
+              zIndex: 10,
+            }}
+          >
+            <div
+              className="home__img"
+              style={{
+                backgroundImage: `url(${myImage})`,
+              }}
+            ></div>
+            {/* <Lanyard position={[0, 0, 20]} gravity={[0, -40, 0]} /> */}
+          </Grid>
+        </Grid>
+      </Container>
+    </Box>
     // <Box
     //   sx={{
     //     position: "relative",
@@ -419,56 +419,56 @@ export default function Hero() {
     //   </div>
     // </div>
 
-    <div className="min-h-screen flex items-center justify-center relative">
-      <div className="container mx-auto">
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-8 items-center relative">
-          <Lanyard />
+    // <div className="min-h-screen flex items-center justify-center relative">
+    //   <div className="container mx-auto">
+    //     <div className="grid grid-cols-1 md:grid-cols-2 gap-8 items-center relative">
+    //       <Lanyard />
 
-          {/* <div className="text-center md:text-left mt-[-120px] md:mt-0"> */}
-          <div className="text-center md:text-left">
+    //       {/* <div className="text-center md:text-left mt-[-120px] md:mt-0"> */}
+    //       <div className="text-center md:text-left">
 
-            <motion.div
-              initial={{ opacity: 0, x: -20 }}
-              animate={{ opacity: 1, x: 0 }}
-              transition={{ duration: 0.8 }}
-            >
-              <motion.div
-                initial={{ opacity: 0, y: 20 }}
-                animate={{ opacity: 1, y: 0 }}
-                whileHover={{ rotateY: 15, rotateX: 15, scale: 1.05 }}
-                whileTap={{ rotateY: 0, rotateX: 0, scale: 0.95 }}
-                transition={{
-                  type: "spring",
-                  stiffness: 300,
-                  damping: 20,
-                  duration: 0.8,
-                }}
-              >
-                <Typography
-                  sx={{ marginTop: 5, fontSize: { xs: "2.6rem", md: "3rem" } }}
-                >
-                  <SplitText text="Hello" delay={60} />
-                </Typography>
-              </motion.div>
-              <Typography sx={{ fontSize: { xs: "1.7rem", md: "2rem" } }}>
-                I'm Gokul,
-              </Typography>
-              <Typography sx={{ fontSize: { xs: "1.8rem", md: "2rem" } }}>
-                a Software{" "}
-                <span style={{ display: "inline-block" }}>
-                  <Typewriter
-                    options={{
-                      strings: [" Developer", " Engineer"],
-                      autoStart: true,
-                      loop: true,
-                    }}
-                  />
-                </span>
-              </Typography>
-            </motion.div>
-          </div>
-        </div>
-      </div>
-    </div>
+    //         <motion.div
+    //           initial={{ opacity: 0, x: -20 }}
+    //           animate={{ opacity: 1, x: 0 }}
+    //           transition={{ duration: 0.8 }}
+    //         >
+    //           <motion.div
+    //             initial={{ opacity: 0, y: 20 }}
+    //             animate={{ opacity: 1, y: 0 }}
+    //             whileHover={{ rotateY: 15, rotateX: 15, scale: 1.05 }}
+    //             whileTap={{ rotateY: 0, rotateX: 0, scale: 0.95 }}
+    //             transition={{
+    //               type: "spring",
+    //               stiffness: 300,
+    //               damping: 20,
+    //               duration: 0.8,
+    //             }}
+    //           >
+    //             <Typography
+    //               sx={{ marginTop: 5, fontSize: { xs: "2.6rem", md: "3rem" } }}
+    //             >
+    //               <SplitText text="Hello" delay={60} />
+    //             </Typography>
+    //           </motion.div>
+    //           <Typography sx={{ fontSize: { xs: "1.7rem", md: "2rem" } }}>
+    //             I'm Gokul,
+    //           </Typography>
+    //           <Typography sx={{ fontSize: { xs: "1.8rem", md: "2rem" } }}>
+    //             a Software{" "}
+    //             <span style={{ display: "inline-block" }}>
+    //               <Typewriter
+    //                 options={{
+    //                   strings: [" Developer", " Engineer"],
+    //                   autoStart: true,
+    //                   loop: true,
+    //                 }}
+    //               />
+    //             </span>
+    //           </Typography>
+    //         </motion.div>
+    //       </div>
+    //     </div>
+    //   </div>
+    // </div>
   );
 }
